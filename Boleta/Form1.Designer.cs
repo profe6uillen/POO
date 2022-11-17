48dfd84fd5af9c9b97895a2cf27afa02b5fe1637
profe6uillen <lui56uillen@gmail.com>
2022-11-17 08:46:04 -0600

    practica 7 1/2

---
+++ Boleta/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Boleta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAlumno = new System.Windows.Forms.Label();
+            this.txtAlumno = new System.Windows.Forms.TextBox();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.txtMateria = new System.Windows.Forms.TextBox();
+            this.lblMateria = new System.Windows.Forms.Label();
+            this.lblNota = new System.Windows.Forms.Label();
+            this.txtNota = new System.Windows.Forms.TextBox();
+            this.dgvNotas = new System.Windows.Forms.DataGridView();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.cboAlumno = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNotas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAlumno
+            // 
+            this.lblAlumno.AutoSize = true;
+            this.lblAlumno.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlumno.Location = new System.Drawing.Point(21, 9);
+            this.lblAlumno.Name = "lblAlumno";
+            this.lblAlumno.Size = new System.Drawing.Size(206, 26);
+            this.lblAlumno.TabIndex = 0;
+            this.lblAlumno.Text = "Nombre del Alumno";
+            // 
+            // txtAlumno
+            // 
+            this.txtAlumno.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAlumno.Location = new System.Drawing.Point(22, 39);
+            this.txtAlumno.Name = "txtAlumno";
+            this.txtAlumno.Size = new System.Drawing.Size(392, 32);
+            this.txtAlumno.TabIndex = 1;
+            this.txtAlumno.Visible = false;
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFecha.Location = new System.Drawing.Point(448, 39);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.ReadOnly = true;
+            this.txtFecha.Size = new System.Drawing.Size(133, 32);
+            this.txtFecha.TabIndex = 2;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.Location = new System.Drawing.Point(477, 9);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(72, 26);
+            this.lblFecha.TabIndex = 3;
+            this.lblFecha.Text = "Fecha";
+            // 
+            // txtMateria
+            // 
+            this.txtMateria.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMateria.Location = new System.Drawing.Point(24, 106);
+            this.txtMateria.Name = "txtMateria";
+            this.txtMateria.Size = new System.Drawing.Size(242, 32);
+            this.txtMateria.TabIndex = 4;
+            // 
+            // lblMateria
+            // 
+            this.lblMateria.AutoSize = true;
+            this.lblMateria.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMateria.Location = new System.Drawing.Point(21, 79);
+            this.lblMateria.Name = "lblMateria";
+            this.lblMateria.Size = new System.Drawing.Size(84, 26);
+            this.lblMateria.TabIndex = 5;
+            this.lblMateria.Text = "Materia";
+            // 
+            // lblNota
+            // 
+            this.lblNota.AutoSize = true;
+            this.lblNota.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota.Location = new System.Drawing.Point(290, 77);
+            this.lblNota.Name = "lblNota";
+            this.lblNota.Size = new System.Drawing.Size(124, 26);
+            this.lblNota.TabIndex = 7;
+            this.lblNota.Text = "Calificación";
+            this.lblNota.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtNota
+            // 
+            this.txtNota.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNota.Location = new System.Drawing.Point(313, 106);
+            this.txtNota.Name = "txtNota";
+            this.txtNota.Size = new System.Drawing.Size(78, 32);
+            this.txtNota.TabIndex = 6;
+            // 
+            // dgvNotas
+            // 
+            this.dgvNotas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNotas.Location = new System.Drawing.Point(28, 195);
+            this.dgvNotas.Name = "dgvNotas";
+            this.dgvNotas.Size = new System.Drawing.Size(553, 172);
+            this.dgvNotas.TabIndex = 8;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(32, 151);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(110, 34);
+            this.btnGuardar.TabIndex = 9;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(173, 151);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(110, 34);
+            this.btnEditar.TabIndex = 10;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.Location = new System.Drawing.Point(315, 151);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(110, 34);
+            this.btnBorrar.TabIndex = 11;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(471, 151);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(110, 34);
+            this.btnSalir.TabIndex = 12;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // cboAlumno
+            // 
+            this.cboAlumno.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboAlumno.FormattingEnabled = true;
+            this.cboAlumno.Items.AddRange(new object[] {
+            "Luis Angel Guillén Hernández",
+            "Pancho López Suárez",
+            "Rosario Cruz Cárdenas",
+            "Clodomiro Pancrasio Luna"});
+            this.cboAlumno.Location = new System.Drawing.Point(22, 38);
+            this.cboAlumno.Name = "cboAlumno";
+            this.cboAlumno.Size = new System.Drawing.Size(392, 33);
+            this.cboAlumno.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(610, 379);
+            this.Controls.Add(this.cboAlumno);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.dgvNotas);
+            this.Controls.Add(this.lblNota);
+            this.Controls.Add(this.txtNota);
+            this.Controls.Add(this.lblMateria);
+            this.Controls.Add(this.txtMateria);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.txtFecha);
+            this.Controls.Add(this.txtAlumno);
+            this.Controls.Add(this.lblAlumno);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNotas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAlumno;
+        private System.Windows.Forms.TextBox txtAlumno;
+        private System.Windows.Forms.TextBox txtFecha;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.TextBox txtMateria;
+        private System.Windows.Forms.Label lblMateria;
+        private System.Windows.Forms.Label lblNota;
+        private System.Windows.Forms.TextBox txtNota;
+        private System.Windows.Forms.DataGridView dgvNotas;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ComboBox cboAlumno;
+    }
+}
+
